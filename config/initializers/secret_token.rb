75946e463b4aef57acbd3ba85956b1e01a4f19dd
Alex Phinizy <AlexPhinizy@gmail.com>
2014-02-27 17:06:53 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reunion::Application.config.secret_key_base = '41b40ca9577af3d56980d228124f5530a89e2b8f14319e3ce4c73c224c604bf851e02ee534c89e611224a3ca902a0cc2339da6089ffb9b057080df06b6650bd5'
